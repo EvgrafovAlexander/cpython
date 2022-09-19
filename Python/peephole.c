edfdfa03b663530b592b85279a7b07eb499b2822
EvgrafovAlexander <nemesis92@me.com>
2022-09-19 23:28:35 +0500

    # python 3.8 try test return LOAD_OTUS

--- Python/peephole.c
+++ Python/peephole.c
@@ -310,10 +310,10 @@ PyCode_Optimize(PyObject *code, PyObject* consts, PyObject *names,
                 }
                 /* В случае выполнения условий выполняем оптимизацию */
                 //codestr[op_start] = LOAD_OTUS;
-                codestr[i] = NOP;
-                codestr[i+1] = NOP;
-                codestr[i+2] = NOP;
-                codestr[i+3] = LOAD_OTUS;
+                codestr[nexti] = LOAD_OTUS;
+                //codestr[i+1] = NOP;
+                //codestr[i+2] = NOP;
+                //codestr[i+3] = LOAD_OTUS;
                 //fill_nops(codestr, op_start + 1, nexti + 1);
                 break;
 
